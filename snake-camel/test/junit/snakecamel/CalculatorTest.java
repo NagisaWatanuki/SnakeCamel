e3681f77efed230446c3de252f113896eec99504
g14957wn <g14957wn@s30303.tsuda.ac.jp>
2016-05-12 10:32:33 +0900

    エラーを修正しました

--- snake-camel/test/junit/snakecamel/CalculatorTest.java
+++ snake-camel/test/junit/snakecamel/CalculatorTest.java
@@ -1,14 +1,28 @@
 package junit.snakecamel;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
+import snakecamel.SnakeCamelUtil;
+
 public class CalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void スネークケースの文字列をキャメルケースの文字列に変換する() {
+		SnakeCamelUtil snake = new SnakeCamelUtil();
+		String expected = "AbcDef";//変換後
+		String actual = snake.snakeToCamelcase("abc_def");//変換前
+		assertThat(actual,is(expected));
+	}
+	@Test
+	public void キャメルケースの文字列をスネークケースの文字列に変換する() {
+		SnakeCamelUtil camel = new SnakeCamelUtil();
+		String expected = "abc_def";//正解
+		String actual = camel.camelToSnakecase("AbcDef");
+		assertThat(actual,is(expected));
 	}
 
 }
